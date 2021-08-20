724d3b6c6343a0f96c2d8373db7d56a63ebf4513
Abigail18kiki <acrespin282@gmail.com>
2021-08-20 17:07:41 -0300

    Creamos algo con teorema pitagoras

---
+++ ejercicio1/index.js
@@ -0,0 +1,9 @@
+const calcular = () => {
+    const co = document.getElementById ("inp_co").value;
+    const ca = document.getElementById ("inp_ca").value;
+    const resultado = co*ca;
+    document.getElementById ("inp_calcular").value= resultado;
+
+}
+const boton = document.getElementById ("btn_calcular");
+boton.addEventListener("click", calcular);
