3aedff39553a6e49ac139ff1a4fee74d99618386
Abigail18kiki <acrespin282@gmail.com>
2021-08-20 17:08:32 -0300

    Otra vez lo subo

--- ejercicio1/index.js
+++ ejercicio1/index.js
@@ -2,7 +2,7 @@ const calcular = () => {
     const co = document.getElementById ("inp_co").value;
     const ca = document.getElementById ("inp_ca").value;
     const resultado = co*ca;
-    document.getElementById ("inp_calcular").value= resultado;
+    document.getElementById ("inp_calcular").value= resultado; 
 
 }
 const boton = document.getElementById ("btn_calcular");
